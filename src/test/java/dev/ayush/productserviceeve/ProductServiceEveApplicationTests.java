2628d9c18b55d9d8451bae579e2f665b3f49ff93
ayush2978 <ayush2978@gmail.com>
2025-01-14 22:31:18 +0530

    commenting test case

--- src/test/java/dev/ayush/productserviceeve/ProductServiceEveApplicationTests.java
+++ src/test/java/dev/ayush/productserviceeve/ProductServiceEveApplicationTests.java
@@ -34,19 +34,19 @@ class ProductServiceEveApplicationTests {
 //        mentor.setNumberOfMentees(10);
 //        mentor.setNumberOfSessions(20);
 //        mentorRepository.save(mentor);
-//
-        Instructor instructor = new Instructor();
-        instructor.setEmail("ashish@gmail.com");
-        instructor.setPassword("ashish1234");
-        instructor.setHandsome(true);
-        instructorReporistorySingleClass.save(instructor);
-//
-//
-        TA ta = new TA();
-        ta.setEmail("saurav@gmail.com");
-        ta.setPassword("saurav1234");
-        ta.setAverageRating(4.5);
-        taRepositorySingleClass.save(ta);
+////
+//        Instructor instructor = new Instructor();
+//        instructor.setEmail("ashish@gmail.com");
+//        instructor.setPassword("ashish1234");
+//        instructor.setHandsome(true);
+//        instructorReporistorySingleClass.save(instructor);
+////
+////
+//        TA ta = new TA();
+//        ta.setEmail("saurav@gmail.com");
+//        ta.setPassword("saurav1234");
+//        ta.setAverageRating(4.5);
+//        taRepositorySingleClass.save(ta);
 
     }
 }
